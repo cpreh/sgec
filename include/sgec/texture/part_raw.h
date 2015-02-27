e144a1384bf8733a95f558a4ac34b47d946e230f
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-02-27 16:12:06 +0100

    Added part_raw with rect

--- include/sgec/texture/part_raw.h
+++ include/sgec/texture/part_raw.h
@@ -5,6 +5,7 @@
 #include <sgec/detail/footer.h>
 #include <sgec/detail/header.h>
 #include <sgec/renderer/texture/planar_fwd.h>
+#include <sgec/renderer/texture/size.h>
 #include <sgec/texture/part_fwd.h>
 
 
@@ -14,6 +15,16 @@ SGEC_DETAIL_SYMBOL
 struct sgec_texture_part *
 sgec_texture_part_raw(
 	struct sgec_renderer_texture_planar *
+);
+
+SGEC_DETAIL_SYMBOL
+struct sgec_texture_part *
+sgec_texture_part_raw_rect(
+	struct sgec_renderer_texture_planar *,
+	sgec_renderer_texture_size x,
+	sgec_renderer_texture_size y,
+	sgec_renderer_texture_size w,
+	sgec_renderer_texture_size h
 );
 
 SGEC_DETAIL_FOOTER
--- src/texture/part_raw.cpp
+++ src/texture/part_raw.cpp
@@ -1,6 +1,8 @@
+#include <sgec/renderer/texture/size.h>
 #include <sgec/impl/renderer/texture/planar.hpp>
 #include <sgec/impl/texture/part.hpp>
 #include <sgec/texture/part_raw.h>
+#include <sge/renderer/lock_rect.hpp>
 #include <sge/texture/part_raw_ref.hpp>
 #include <fcppt/make_unique_ptr.hpp>
 
@@ -18,6 +20,44 @@ try
 				sge::texture::part_raw_ref
 			>(
 				_texture->get()
+			)
+		);
+}
+catch(
+	...
+)
+{
+	return
+		nullptr;
+}
+
+extern "C"
+struct sgec_texture_part *
+sgec_texture_part_raw_rect(
+	struct sgec_renderer_texture_planar *const _texture,
+	sgec_renderer_texture_size const _x,
+	sgec_renderer_texture_size const _y,
+	sgec_renderer_texture_size const _w,
+	sgec_renderer_texture_size const _h
+)
+try
+{
+	return
+		new sgec_texture_part(
+			fcppt::make_unique_ptr<
+				sge::texture::part_raw_ref
+			>(
+				_texture->get(),
+				sge::renderer::lock_rect{
+					sge::renderer::lock_rect::vector{
+						_x,
+						_y
+					},
+					sge::renderer::lock_rect::dim{
+						_w,
+						_h
+					}
+				}
 			)
 		);
 }

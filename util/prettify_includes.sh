339d2798128d2a635d3c852bea306a18824a1336
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-10-02 19:39:17 +0200

    Remove majutsu

--- util/prettify_includes.sh
+++ util/prettify_includes.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == 'run' ]]; then
 	shift
-	prettify_includes.py --reserved-prefix sgec --reserved-prefix sge --reserved-prefix majutsu --reserved-prefix awl --reserved-prefix mizuiro --reserved-prefix fcppt  "$@" 2>>/tmp/prettify_errors
+	prettify_includes.py --reserved-prefix sgec --reserved-prefix sge --reserved-prefix awl --reserved-prefix mizuiro --reserved-prefix fcppt  "$@" 2>>/tmp/prettify_errors
 else
 	find examples src include \( -name '*.hpp' -o -name '*.cpp' \) \
 	| xargs $0 run

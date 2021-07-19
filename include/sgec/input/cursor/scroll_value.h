56abaea5ff39edbde68987704e96b120c02c92a4
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-07-19 11:11:38 +0200

    Fix cursor/mouse int types

--- include/sgec/input/cursor/scroll_value.h
+++ include/sgec/input/cursor/scroll_value.h
@@ -1,9 +1,12 @@
 #ifndef SGEC_INPUT_CURSOR_SCROLL_VALUE_H_INCLUDED
 #define SGEC_INPUT_CURSOR_SCROLL_VALUE_H_INCLUDED
 
+#include <fcppt/config/external_begin.h>
+#include <stdint.h>
+#include <fcppt/config/external_end.h>
 
 typedef
-long
+int64_t
 sgec_input_cursor_scroll_value;
 
 #endif
--- include/sgec/input/mouse/axis_value.h
+++ include/sgec/input/mouse/axis_value.h
@@ -1,9 +1,12 @@
 #ifndef SGEC_INPUT_MOUSE_AXIS_VALUE_H_INCLUDED
 #define SGEC_INPUT_MOUSE_AXIS_VALUE_H_INCLUDED
 
+#include <fcppt/config/external_begin.h>
+#include <stdint.h>
+#include <fcppt/config/external_end.h>
 
 typedef
-long
+int64_t
 sgec_input_mouse_axis_value;
 
 #endif

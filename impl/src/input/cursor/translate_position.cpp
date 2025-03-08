4b94d37f685fc573a7c95d3645b0113ec176aae0
Carl Philipp Reh <carlphilippreh@gmail.com>
2025-03-08 13:11:30 +0100

    Fix some clang-tidy warnings

--- impl/src/input/cursor/translate_position.cpp
+++ impl/src/input/cursor/translate_position.cpp
@@ -5,5 +5,5 @@
 sgec_input_cursor_position
 sgec::impl::input::cursor::translate_position(sge::input::cursor::position const &_position)
 {
-  return sgec_input_cursor_position{_position.x(), _position.y()};
+  return sgec_input_cursor_position{.pos_x = _position.x(), .pos_y = _position.y()};
 }
--- impl/src/input/focus/translate_text_event.cpp
+++ impl/src/input/focus/translate_text_event.cpp
@@ -2,6 +2,9 @@
 #include <sgec/impl/input/focus/translate_text_event.hpp>
 #include <sgec/input/focus/text_event.h>
 #include <sge/input/focus/event/text.hpp>
+#include <fcppt/preprocessor/ignore_unsafe_buffer_usage_in_libc_call.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <cwchar>
 #include <fcppt/config/external_end.hpp>
@@ -11,7 +14,10 @@ sgec::impl::input::focus::translate_text_event(sge::input::focus::event::text co
 {
   wchar_t *const text{new wchar_t[_event.get().size() + 1U]};
 
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_IGNORE_UNSAFE_BUFFER_USAGE_IN_LIBC_CALL
   std::wcscpy(text, _event.get().c_str());
+FCPPT_PP_POP_WARNING
 
   return sgec_input_focus_text_event{
       .id = sgec::impl::input::focus::make_id(*_event.focus()), .text = text};
